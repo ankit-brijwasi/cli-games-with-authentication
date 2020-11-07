f081b09410fd506c2eb9fe9c4a7780db06eede20
ankit-brijwasi <abrijwasi1@gmail.com>
2020-11-07 11:53:16 +0530

    schema function

--- utils/database_functions.py
+++ utils/database_functions.py
@@ -5,11 +5,43 @@ import sqlite3
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASE_PATH = os.path.join(BASE_DIR, 'sqlite3.db')
 
+
+class Schema:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def create_table(self, table_name: str, fields_list: list):
+        '''Create a table
+        table_name = "Dummy"
+        field_list = [
+            {"id": "INT PRIMARY KEY NOT NULL"},
+            {"name": "TEXT NOT NULL"},
+        ]
+        '''
+        fields = ""
+
+        for field in fields_list:
+            for key, value in field.items():
+                fields += (str(key) + " " + str(value) + ", ")
+
+        fields = fields[:-2]
+
+        try:
+            self.conn.execute(
+                f"CREATE TABLE IF NOT EXISTS {table_name} ({fields});")
+        except Exception as e:
+            raise str(e)
+
+
 class Database:
     '''All the functions regarding Database connectivity lives in this class'''
 
     def __init__(self):
-        self.connection = sqlite3.connect(DATABASE_PATH)
+        self.conn = sqlite3.connect(DATABASE_PATH)
+        self.schema = Schema(self.conn)
+
+    def migrate_db(self):
+        self.schema.create_table()
 
     def close_db(self):
-        self.connection.close()
+        self.conn.close()
