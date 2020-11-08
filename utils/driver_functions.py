c98ecbbc8560e3419bd3a86c47ef648a1c54d760
ankit-brijwasi <abrijwasi1@gmail.com>
2020-11-08 21:13:37 +0530

    valid_email valid_password get_user_credentails function

--- utils/driver_functions.py
+++ utils/driver_functions.py
@@ -27,3 +27,39 @@ def clear() -> None:
 def get_user_choice() -> int:
     '''fuction to get the user's choice'''
     return int(input(">> "))
+
+
+def valid_email(email: str) -> bool:
+    return True if "@" in email and "." in email else False
+
+
+def valid_password(password: str) -> tuple:
+    if len(password) >= 6 and len(password) <= 15:
+        return False, "Password should be 6 - 15 characters long."
+    elif password.isalpha():
+        return False, "Password contains only alphabets."
+    elif password.isnumeric():
+        return False, "Password contains only numbers."
+    else:
+        return True, None
+
+
+def get_user_credentails(screen: str) -> tuple:
+    '''gets the user credentials'''
+    if screen == "login":
+        email = input("Enter e-mail: ")
+        if valid_email(email):
+            password = input("Enter password: ")
+            return email, password
+        raise("InvalidEmailError: Please enter a valid email address")
+
+    elif screen == "signup":
+        name = input("Enter your name: ")
+        email = input("Enter e-mail: ")
+        if not valid_email(email):
+            raise("InvalidEmailError: Please enter a valid email address")
+        password = input("Enter password: ")
+        is_valid, reason = valid_password(password)
+        if not is_valid:
+            raise(f"InvalidPasswordError: {reason}")
+        return name, email, password
